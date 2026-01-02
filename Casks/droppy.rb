13521115b49bff1e70d55fee90af6fa986d21ac6
iordv <jordy.spruit@newyorkpizza.nl>
2026-01-02 18:56:52 +0100

    Update SHA for macOS 26.0 build

--- Casks/droppy.rb
+++ Casks/droppy.rb
@@ -1,6 +1,6 @@
 cask "droppy" do
   version "1.0"
-  sha256 "b189900bf39f24b5ca259fb9fdd0260fa14972b42584224e0e0e6936e422079a"
+  sha256 "a17f852440e6d798779ba00060d7b7b1213eff798a99688ac6c9c6bd3d192d7a"
 
   url "https://raw.githubusercontent.com/iordv/Droppy/main/Droppy.dmg"
   name "Droppy"
