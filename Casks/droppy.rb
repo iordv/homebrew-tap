760e487107c809221b95b83c12107e588e9d2138
iordv <jordy.spruit@newyorkpizza.nl>
2026-01-03 21:33:42 +0100

    style: add ASCII art logo

--- Casks/droppy.rb
+++ Casks/droppy.rb
@@ -9,6 +9,18 @@ cask "droppy" do
 
   app "Droppy.app"
 
+  caveats <<~EOS
+    ____                             
+   / __ \_________  ____  ____  __  __
+  / / / / ___/ __ \/ __ \/ __ \/ / / /
+ / /_/ / /  / /_/ / /_/ / /_/ / /_/ / 
+/_____/_/   \____/ .___/ .___/\__, /  
+                /_/   /_/    /____/   
+
+    Thank you for installing Droppy! 
+    The ultimate drag-and-drop file shelf for macOS.
+  EOS
+
   zap trash: [
     "~/Library/Application Support/Droppy",
     "~/Library/Preferences/iordv.Droppy.plist",
