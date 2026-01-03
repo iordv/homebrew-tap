788dd81eb3b11266ac1c6d79004ea681f761f37d
iordv <jordy.spruit@newyorkpizza.nl>
2026-01-03 20:31:30 +0100

    Fix URL for v2.0.4

--- Casks/droppy.rb
+++ Casks/droppy.rb
@@ -2,7 +2,7 @@ cask "droppy" do
   version "2.0.4"
   sha256 "f23febdea82c94911bc196134d1fca49e3796f5410084871e2b542445d9e76a8"
 
-  url "https://raw.githubusercontent.com/iordv/Droppy/main/Droppy-2.0.2.dmg"
+  url "https://raw.githubusercontent.com/iordv/Droppy/main/Droppy-2.0.4.dmg"
   name "Droppy"
   desc "Drag and drop file shelf for macOS"
   homepage "https://github.com/iordv/Droppy"
