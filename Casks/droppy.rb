b9ac2f0f5e668427e9cd2221a728fa261360e1fb
iordv <jordy.spruit@newyorkpizza.nl>
2026-01-04 04:56:01 +0100

    Update Droppy to v2.6.5

--- Casks/droppy.rb
+++ Casks/droppy.rb
@@ -1,8 +1,8 @@
 cask "droppy" do
-  version "2.6.4"
-  sha256 "1cf3607285e3f890b7db848551d05355932eb39d2ebff42bf0d4c75fa74f33b7"
+  version "2.6.5"
+  sha256 "fd1bdb249355ee67ae68515f821b69e4b7f68a64a72fc5ed82104847775cd29a"
 
-  url "https://raw.githubusercontent.com/iordv/Droppy/main/Droppy-2.6.4.dmg"
+  url "https://raw.githubusercontent.com/iordv/Droppy/main/Droppy-2.6.5.dmg"
   name "Droppy"
   desc "Drag and drop file shelf for macOS"
   homepage "https://github.com/iordv/Droppy"
