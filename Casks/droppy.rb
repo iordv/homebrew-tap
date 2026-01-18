3712705ec5ad3d153ca4a2db75715fa002d42298
iordv <jordy.spruit@newyorkpizza.nl>
2026-01-18 23:09:56 +0100

    Update Droppy to v8.3.11

--- Casks/droppy.rb
+++ Casks/droppy.rb
@@ -1,8 +1,8 @@
 cask "droppy" do
-  version "8.3.10"
-  sha256 "c24291b61d5942ae9db5014e1da9657d29d5aa2a686c7eb63781e3ab93bcca9b"
+  version "8.3.11"
+  sha256 "e89ac36ae93c37094b7b3461077802b08d0762e53a8a8b32315f309ff29581e8"
 
-  url "https://github.com/iordv/Droppy/releases/download/v8.3.10/Droppy-8.3.10.dmg"
+  url "https://github.com/iordv/Droppy/releases/download/v8.3.11/Droppy-8.3.11.dmg"
   name "Droppy"
   desc "Drag and drop file shelf for macOS"
   homepage "https://github.com/iordv/Droppy"
